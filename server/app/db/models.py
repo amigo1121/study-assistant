b132146ce02e7b661cb6c2b4e11b1c4074e01412
amigo1121 <hoainam.hk14.cqt@gmail.com>
2023-03-31 01:15:09 +0200

    chore: refactor database

--- server/app/db/models.py
+++ server/app/db/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
 from datetime import datetime
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 
 from .database import Base
 
@@ -12,43 +13,5 @@ class User(Base):
     username=Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
 
-    items = relationship("Item", back_populates="owner")
-
-class Item(Base):
-    __tablename__ = "items"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
-
-    owner = relationship("User", back_populates="items")
-
-
-class Task(Base):
-    __tablename__ = 'tasks'
-
-    id = Column(Integer, primary_key=True)
-    title = Column(String)
-    description = Column(String)
-    created_at = Column(DateTime, default=datetime.utcnow)
-    modified_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    due_date = Column(DateTime)
-    priority = Column(String)
-    credit = Column(Integer)
-    subtasks = relationship('Subtask', back_populates='task')
-
-class Subtask(Base):
-    __tablename__ = 'subtasks'
-
-    id = Column(Integer, primary_key=True)
-    title = Column(String)
-    description = Column(String)
-    created_at = Column(DateTime, default=datetime.utcnow)
-    modified_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    due_date = Column(DateTime)
-    priority = Column(String)
-    credit = Column(Integer)
-    task_id = Column(Integer, ForeignKey('tasks.id'))
-    task = relationship('Task', back_populates='subtasks')
--- server/app/db/schemas.py
+++ server/app/db/schemas.py
@@ -2,23 +2,6 @@ from pydantic import BaseModel
 from typing import Optional, List
 from datetime import datetime
 
-class ItemBase(BaseModel):
-    title: str
-    description: str | None = None
-
-
-class ItemCreate(ItemBase):
-    pass
-
-
-class Item(ItemBase):
-    id: int
-    owner_id: int
-
-    class Config:
-        orm_mode = True
-
-
 class UserBase(BaseModel):
     email: str
     username: str
@@ -35,66 +18,7 @@ class UserLogin(BaseModel):
 class User(UserBase):
     id: int
     is_active: bool
-    items: list[Item] = []
-
-    class Config:
-        orm_mode = True
-
-
-class TaskBase(BaseModel):
-    title: str
-    description: str
-    due_date: Optional[datetime] = None
-    priority: Optional[str] = None
-    credit: Optional[int] = None
-
-class TaskCreate(TaskBase):
-    pass
-
-class TaskUpdate(TaskBase):
-    pass
-
-class Task(TaskBase):
-    id: int
     created_at: datetime
-    modified_at: datetime
 
     class Config:
         orm_mode = True
-
-class TaskWithSubtasks(Task):
-    subtasks: List['Subtask'] = []
-
-class TaskCreateWithSubtasks(TaskCreate):
-    subtasks: Optional[List['SubtaskCreate']] = []
-
-class TaskUpdateWithSubtasks(TaskUpdate):
-    subtasks: Optional[List['SubtaskUpdate']] = []
-
-class TaskDelete(TaskBase):
-    id: int
-
-class SubtaskBase(BaseModel):
-    title: str
-    description: str
-    due_date: Optional[datetime] = None
-    priority: Optional[str] = None
-    credit: Optional[int] = None
-
-class SubtaskCreate(SubtaskBase):
-    task_id: int
-
-class SubtaskUpdate(SubtaskBase):
-    pass
-
-class Subtask(SubtaskBase):
-    id: int
-    created_at: datetime
-    modified_at: datetime
-    task_id: int
-
-    class Config:
-        orm_mode = True
-
-class SubtaskDelete(SubtaskBase):
-    id: int
