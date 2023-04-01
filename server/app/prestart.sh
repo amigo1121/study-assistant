3e9a3adb5b85cc7a8411536971f323f36545ac0d
amigo1121 <hoainam.hk14.cqt@gmail.com>
2023-04-01 12:39:51 +0200

    speedup connect between server and database

--- server/app/prestart.sh
+++ server/app/prestart.sh
@@ -1,9 +1,8 @@
 #! /usr/bin/env bash
 
-# # Let the DB start
-# python /server/app/app/server_pre_start.py
-
-# # Run migrations
-# alembic upgrade head
-
+# Let the DB start
 echo "Waiting for postgres..."
+python ./app/server_pre_start.py
+
+# Run migrations
+alembic upgrade head
