63bec5d796a8d4bcf432fbe8fcd0bb2f4753028e
amigo1121 <hoainam.hk14.cqt@gmail.com>
2023-04-02 14:37:03 +0200

    simple oauth

--- server/app/app/api/api_v1/api.py
+++ server/app/app/api/api_v1/api.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
-from .endpoints import users
+from .endpoints import users, security
+
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(security.router, prefix="/oauth", tags=["oauth"])
---
+++ server/app/app/api/api_v1/endpoints/security.py
@@ -0,0 +1,101 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from typing import Annotated
+from datetime import datetime
+from pydantic import BaseModel
+import logging
+
+router = APIRouter()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="oauth/token")
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    user = get_user(fake_users_db, token)
+    return user
+
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_full_name(firstname: str, lastname: str):
+    return firstname + " " + lastname
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive User")
+    return current_user
+
+
+@router.get("/users/me")
+async def read_user_me(current_user: Annotated[User, Depends(get_current_active_user)]):
+    return current_user
+
+
+@router.get("/test/depend")
+async def idk(some_arg: Annotated[str, Depends(get_full_name)]):
+    return "Your full name is: " + some_arg
+
+
+@router.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Sai kia thang loz")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="sai mk")
+
+    return {"access_token": user.username, "token_type": "bearer"}
