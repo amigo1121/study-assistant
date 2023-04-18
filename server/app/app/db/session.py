a6fe6fc96c5c3a49c082bfca8de08b80db84433b
amigo1121 <hoainam.hk14.cqt@gmail.com>
2023-04-18 21:23:33 +0200

    add user table

--- server/app/app/db/session.py
+++ server/app/app/db/session.py
@@ -2,14 +2,16 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DB_USER=os.environ.get("DB_USER")
-DB_HOST=os.environ.get("DB_HOST")
-DB_PASSWORD=os.environ.get("DB_PASSWORD")
-DB_PORT=os.environ.get("DB_PORT")
-DB_DATABASE=os.environ.get("DB_DATABASE")
+DB_USER = os.environ.get("DB_USER")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_PORT = os.environ.get("DB_PORT")
+DB_DATABASE = os.environ.get("DB_DATABASE")
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
+DATABASE_URL = (
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
+)
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker (autocommit=False,autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
