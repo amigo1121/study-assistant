61fac35d32708190b0db9f7cd8389487dabb43a6
amigo1121 <hoainam.hk14.cqt@gmail.com>
2023-04-18 06:21:37 +0200

    style Register

--- client/src/main.ts
+++ client/src/main.ts
@@ -8,14 +8,18 @@ import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
 import Dialog from 'primevue/dialog';
 import { useAuthStore } from './stores/auth';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(router);
 app.use(pinia);
 app.use(PrimeVue);
+app.use(ToastService);
 app.component('QuillEditor', QuillEditor);
 app.component('Dialog', Dialog);
+app.component('Toast', Toast);
 console.log("init");
 
 
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,23 +1,47 @@
 <template>
-    <h3>register</h3>
-    <form>
-        <input type="text" v-model="username" placeholder="username" />
-        <input type="text" v-model="email" placeholder="email" />
-        <input type="password" v-model="password" placeholder="password" />
-        <button type="button" @click="register">Register</button>
-    </form>
-
-    <button type="button" @click="test">Test</button>
+    <div class="container">
+        <Card id="card">
+            <template #title> <div class="flex justify-content-center text-4xl">
+                Sign Up
+            </div>
+        </template>
+            <template #content>
+                <label for="username" class="block text-900 text-xl font-medium mb-2">Username</label>
+                <InputText id="username" type="text" placeholder="Username" class="w-full md:w-30rem mb-3"
+                    style="padding: 1rem" v-model="username" />
+                <label for="email" class="block text-900 text-xl font-medium mb-2">Email</label>
+                <InputText id="email" type="text" placeholder="Email" class="w-full md:w-30rem mb-3" style="padding: 1rem"
+                    v-model="email" />
+                <label for="password1" class="block text-900 font-medium text-xl mb-2">Password</label>
+                <Password id="password1" v-model="password" placeholder="Password" :toggleMask="true" :feedback="false"
+                    class="w-full mb-3" inputClass="w-full" :inputStyle="{padding:'1rem'}"></Password>
+                <label for="password2" class="block text-900 font-medium text-xl mb-2">Confirm Password</label>
+                <Password id="password2" v-model="confirmPassword" placeholder="Confirm Password" :toggleMask="true"
+                    :feedback="false" class="w-full mb-3" inputClass="w-full" :inputStyle="{padding:'1rem'}"></Password>
+                <Button label="Sign up" class="w-full p-3 text-xl mt-2" @click="register"></Button>
+                <div class="w-100 flex align-items-center justify-content-center mt-3">
+                    <router-link to="login" class="font-medium no-underline ml-2 text-right cursor-pointer"
+                        style="color: var(--primary-color)">Already have a account? Log in</router-link>
+                </div>
+            </template>
+        </Card>
+    </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 import type { Ref } from 'vue';
 import { useAuthStore } from '@/stores/auth';
+import Card from 'primevue/card';
+import InputText from 'primevue/inputtext';
+import Password from 'primevue/password';
+import Button from 'primevue/button';
+import { useToast } from "primevue/usetoast";
 
 const username: Ref<string> = ref('');
 const email: Ref<string> = ref('');
 const password: Ref<string> = ref('');
+const confirmPassword: Ref<string> = ref('');
 const authStore = useAuthStore();
 
 const register = async () => {
@@ -30,6 +54,31 @@ const register = async () => {
         console.log('Invalid credentials');
     }
 };
+
+
+
+const toast = useToast();
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.p-card {
+    padding: 20px;
+    max-width: 500px;
+    position: absolute;
+    margin: auto;
+    inset: 0;
+    height: fit-content;
+}
+
+.p-card-title {
+    text-align: center;
+}
+
+.input-container {
+    margin-bottom: 2rem;
+}
+
+.p-inputtext {
+    width: 100%;
+}
+</style>
