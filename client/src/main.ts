97081be1e6716b0542dadc55ac76fa042f66ae90
amigo1121 <hoainam.hk14.cqt@gmail.com>
2023-04-18 06:55:40 +0200

    style login

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,10 +6,18 @@ import PrimeVue from 'primevue/config';
 import '@/assets/styles.scss';
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
+import Button from 'primevue/button';
+import Checkbox from 'primevue/checkbox';
 import Dialog from 'primevue/dialog';
-import { useAuthStore } from './stores/auth';
+import InputText from 'primevue/inputtext';
 import Toast from 'primevue/toast';
+import Password from 'primevue/password';
+
+
+import { useAuthStore } from './stores/auth';
 import ToastService from 'primevue/toastservice';
+
 const app = createApp(App);
 const pinia = createPinia();
 
@@ -17,9 +25,16 @@ app.use(router);
 app.use(pinia);
 app.use(PrimeVue);
 app.use(ToastService);
+
+app.component('Button', Button);
 app.component('QuillEditor', QuillEditor);
 app.component('Dialog', Dialog);
 app.component('Toast', Toast);
+app.component('InputText', InputText);
+app.component('Checkbox', Checkbox);
+app.component('Password', Password);
+
+
 console.log("init");
 
 
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,30 +1,37 @@
 <template>
-    <div class="container">
-        <Card id="card">
-            <template #title> <div class="flex justify-content-center text-4xl">
-                Sign Up
-            </div>
-        </template>
-            <template #content>
-                <label for="username" class="block text-900 text-xl font-medium mb-2">Username</label>
-                <InputText id="username" type="text" placeholder="Username" class="w-full md:w-30rem mb-3"
-                    style="padding: 1rem" v-model="username" />
-                <label for="email" class="block text-900 text-xl font-medium mb-2">Email</label>
-                <InputText id="email" type="text" placeholder="Email" class="w-full md:w-30rem mb-3" style="padding: 1rem"
-                    v-model="email" />
-                <label for="password1" class="block text-900 font-medium text-xl mb-2">Password</label>
-                <Password id="password1" v-model="password" placeholder="Password" :toggleMask="true" :feedback="false"
-                    class="w-full mb-3" inputClass="w-full" :inputStyle="{padding:'1rem'}"></Password>
-                <label for="password2" class="block text-900 font-medium text-xl mb-2">Confirm Password</label>
-                <Password id="password2" v-model="confirmPassword" placeholder="Confirm Password" :toggleMask="true"
-                    :feedback="false" class="w-full mb-3" inputClass="w-full" :inputStyle="{padding:'1rem'}"></Password>
-                <Button label="Sign up" class="w-full p-3 text-xl mt-2" @click="register"></Button>
-                <div class="w-100 flex align-items-center justify-content-center mt-3">
-                    <router-link to="login" class="font-medium no-underline ml-2 text-right cursor-pointer"
-                        style="color: var(--primary-color)">Already have a account? Log in</router-link>
+    <div class="surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
+        <div class="flex flex-column align-items-center justify-content-center">
+            <div
+                style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
+                <div class="w-full surface-card py-8 px-5 sm:px-8" style="border-radius: 53px">
+                    <div class="text-center mb-5">
+                        <span class="text-4xl font-medium">Sign up</span>
+                    </div>
+                    <div>
+                        <label for="username" class="block text-900 text-xl font-medium mb-2">Username</label>
+                        <InputText id="username" type="text" placeholder="Username" class="w-full md:w-30rem mb-3"
+                            style="padding: 1rem" v-model="username" />
+                        <label for="email" class="block text-900 text-xl font-medium mb-2">Email</label>
+                        <InputText id="email" type="text" placeholder="Email" class="w-full md:w-30rem mb-3"
+                            style="padding: 1rem" v-model="email" />
+                        <label for="password1" class="block text-900 font-medium text-xl mb-2">Password</label>
+                        <Password id="password1" v-model="password" placeholder="Password" :toggleMask="true"
+                            :feedback="false" class="w-full mb-3" inputClass="w-full" :inputStyle="{ padding: '1rem' }">
+                        </Password>
+                        <label for="password2" class="block text-900 font-medium text-xl mb-2">Confirm Password</label>
+                        <Password id="password2" v-model="confirmPassword" placeholder="Confirm Password" :toggleMask="true"
+                            :feedback="false" class="w-full mb-3" inputClass="w-full" :inputStyle="{ padding: '1rem' }">
+                        </Password>
+                        <Button label="Sign up" class="w-full p-3 text-xl mt-2" @click="register"></Button>
+                        <div class="w-100 flex align-items-center justify-content-center mt-3">
+                            <router-link to="login" class="font-medium no-underline ml-2 text-right cursor-pointer"
+                                style="color: var(--primary-color)">Already have a account? Log in</router-link>
+                        </div>
+                    </div>
+
                 </div>
-            </template>
-        </Card>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -32,10 +39,6 @@
 import { ref } from 'vue';
 import type { Ref } from 'vue';
 import { useAuthStore } from '@/stores/auth';
-import Card from 'primevue/card';
-import InputText from 'primevue/inputtext';
-import Password from 'primevue/password';
-import Button from 'primevue/button';
 import { useToast } from "primevue/usetoast";
 
 const username: Ref<string> = ref('');
@@ -61,24 +64,4 @@ const toast = useToast();
 </script>
 
 <style lang="scss" scoped>
-.p-card {
-    padding: 20px;
-    max-width: 500px;
-    position: absolute;
-    margin: auto;
-    inset: 0;
-    height: fit-content;
-}
-
-.p-card-title {
-    text-align: center;
-}
-
-.input-container {
-    margin-bottom: 2rem;
-}
-
-.p-inputtext {
-    width: 100%;
-}
 </style>
