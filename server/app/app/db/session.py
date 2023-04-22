54bac5d115c66f4146b919d27a3ec73b6febd84f
amigo1121 <hoainam.hk14.cqt@gmail.com>
2023-04-23 00:05:12 +0200

    increase query pool size

--- server/app/app/db/session.py
+++ server/app/app/db/session.py
@@ -12,6 +12,6 @@ DATABASE_URL = (
     f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
 )
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_size=100, max_overflow=20)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
