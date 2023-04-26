445e9f8dc61d0787078382385dd5915583cb166f
amigo1121 <hoainam.hk14.cqt@gmail.com>
2023-04-26 08:28:28 +0200

    fix socket.io client

--- client/src/utils/sockets.ts
+++ client/src/utils/sockets.ts
@@ -1,7 +1,6 @@
-import io from "socket.io-client";
 import type { Socket } from "socket.io-client";
+import { Manager } from "socket.io-client"
 import { API_URL } from "./config";
-import { useAuthStore } from "@/stores/auth";
 
 type EventHandler = (data: any) => void;
 
@@ -9,23 +8,14 @@ export class BaseSocket {
   static instance: BaseSocket | null = null;
 
   private socket: Socket;
-
-  constructor(token: string) {
-    this.socket = io(API_URL, {
-      path: "/sio/socket/",
-      auth: { token },
-    });
-    this.socket.on("connect", () => {
-      console.log("Connected to server");
-    });
-    this.socket.on("disconnect", () => {
-      console.log("Disconnected from server");
-    });
+  private manager = null;
+  constructor() {
+    this.manager = new Manager(API_URL,{path: "/sio/socket", autoConnect: false});
   }
 
-  static getInstance(token: string): BaseSocket {
+  static getInstance(): BaseSocket {
     if (!BaseSocket.instance) {
-      BaseSocket.instance = new BaseSocket(token);
+      BaseSocket.instance = new BaseSocket();
     }
     return BaseSocket.instance;
   }
@@ -34,19 +24,54 @@ export class BaseSocket {
     this.socket.emit(event, data);
   }
 
-  on(event: string, handler: EventHandler) {
+  public on(event: string, handler: EventHandler) {
     this.socket.on(event, handler);
   }
 
-  disconnect() {
-    this.socket.disconnect();
+  public off(event: string){
+    if(this.socket)
+    this.socket.off(event);
   }
-  protected subscribe(
-    event: string,
-    handle: (data: Record<string, any>) => void
-  ) {
-    this.socket.on(event, (data) => handle(data));
+
+  public offAll(){
+    if(this.socket)
+    this.socket.off();
   }
+
+  public set(_option) {
+    this.socket = this.manager.socket("/", _option);
+
+    // this.manager = new Manager(API_URL, {autoConnect: false});
+    // this.socket = io(API_URL, {
+    //   path: "/sio/socket/",
+    //   autoConnect: false,
+    //   ..._option
+    // });
+
+    this.socket.on("connect", () => {
+      console.log("Connected to server",this.socket.id);
+    });
+
+  }
+
+  public connect() {
+    this.socket.connect();
+    this.socket.on("disconnect", () => {
+      console.log("Disconnected from server",this.socket.id);
+    });
+  }
+
+  public disconnect() {
+    if (this.socket) this.socket.disconnect();
+    this.socket = null;
+  }
+
+  // protected subscribe(
+  //   event: string,
+  //   handle: (data: Record<string, any>) => void
+  // ) {
+  //   this.socket.on(event, (data) => handle(data));
+  // }
 }
 
-export default BaseSocket
+export default BaseSocket;
