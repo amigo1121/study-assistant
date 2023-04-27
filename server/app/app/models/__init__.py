3199acce18372a6edf75e2ed3b5886d147daa825
amigo1121 <hoainam.hk14.cqt@gmail.com>
2023-04-27 22:57:20 +0200

    add Course table

--- server/app/app/models/__init__.py
+++ server/app/app/models/__init__.py
@@ -1,3 +1,4 @@
 from .user import User
 from .event import Event
+from .course import Course
 from app.db.base_class import Base
--- server/app/app/models/event.py
+++ server/app/app/models/event.py
@@ -5,9 +5,9 @@ from app.db.base_class import Base
 
 
 class Event(Base):
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    owner_id = Column(Integer, ForeignKey("user.id"))
-    start = Column(String)
-    end = Column(String)
+    id = Column(Integer, primary_key=True, index=True, unique=True)
+    title = Column(String, nullable=False)
+    owner_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    start = Column(String, nullable=False)
+    end = Column(String, nullable=False)
     owner = relationship("User", back_populates="events")
