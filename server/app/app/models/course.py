7ef6f2e67423694bc621e92817ca3b582be4c996
amigo1121 <hoainam.hk14.cqt@gmail.com>
2023-04-27 22:57:20 +0200

    add Course table

---
+++ server/app/app/models/course.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Boolean, Column, Integer, String, Date
+from sqlalchemy.orm import relationship
+
+from app.db.base_class import Base
+
+
+class Course(Base):
+    __tablename__ = "courses"
+
+    id = Column(Integer, primary_key=True)
+    code = Column(String(50), unique=True, nullable=False)
+    name = Column(String(255), nullable=False)
+    startDate = Column(Date, nullable=False)
+    endDate = Column(Date, nullable=False)
+    credits = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<Course(code='{self.code}', name='{self.name}', startDate='{self.startDate}', endDate='{self.endDate}', credit={self.credits})>"
