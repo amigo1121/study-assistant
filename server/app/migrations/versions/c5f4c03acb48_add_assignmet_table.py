4f06d4ddd5bdfe01100c17b5c1c070f020cc7243
amigo1121 <hoainam.hk14.cqt@gmail.com>
2023-04-30 02:36:07 +0200

    add assignment table

---
+++ server/app/migrations/versions/c5f4c03acb48_add_assignmet_table.py
@@ -0,0 +1,37 @@
+"""add assignmet table
+
+Revision ID: c5f4c03acb48
+Revises: 5ce1ce425c97
+Create Date: 2023-04-30 00:10:21.915453
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "c5f4c03acb48"
+down_revision = "5ce1ce425c97"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "assignments",
+        sa.Column("id", sa.Integer(), nullable=False, primary_key=True),
+        sa.Column("name", sa.String(length=100), nullable=False),
+        sa.Column("description", sa.Text(), nullable=True),
+        sa.Column(
+            "due_date", sa.DateTime(), nullable=False, server_default=sa.func.now()
+        ),
+        sa.Column(
+            "course_id", sa.Integer(), sa.ForeignKey("courses.id"), nullable=False
+        ),
+    )
+    pass
+
+
+def downgrade() -> None:
+    op.drop_table("assignments")
+    pass
