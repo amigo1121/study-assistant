3464531916c174aee99b45ad438639b3f627ea9c
amigo1121 <hoainam.hk14.cqt@gmail.com>
2023-04-30 02:04:50 +0200

    add enrollment table

--- server/app/migrations/versions/15d948029767_add_user_table.py
+++ server/app/migrations/versions/15d948029767_add_user_table.py
@@ -7,7 +7,17 @@ Create Date: 2023-04-29 20:56:30.034598
 """
 from alembic import op
 import sqlalchemy as sa
+import enum
+from sqlalchemy import Enum
 from sqlalchemy.dialects import postgresql
+
+
+class UserRole(enum.Enum):
+    STUDENT = 1
+    TEACHER = 2
+
+
+user_role = postgresql.ENUM(UserRole, name="user_role")
 
 # revision identifiers, used by Alembic.
 revision = "15d948029767"
@@ -31,12 +41,14 @@ def upgrade() -> None:
         sa.UniqueConstraint("password_hash"),
     )
 
-    user_type = postgresql.ENUM("STUDENT", "TEACHER", name="user_type")
-    user_type.create(op.get_bind())
+    user_role.create(op.get_bind())
     op.add_column(
         "users",
         sa.Column(
-            "role", sa.Enum("STUDENT", "TEACHER", name="user_type"), nullable=False
+            "role",
+            user_role,
+            nullable=False,
+            server_default=UserRole.STUDENT.name,
         ),
     )
     pass
@@ -44,5 +56,4 @@ def upgrade() -> None:
 
 def downgrade():
     op.drop_table("users")
-    user_type = postgresql.ENUM("STUDENT", "TEACHER", name="user_type")
-    user_type.drop(op.get_bind())
+    user_role.drop(op.get_bind())
---
+++ server/app/migrations/versions/5ce1ce425c97_add_enrollment_table.py
@@ -0,0 +1,60 @@
+"""add enrollment table
+
+Revision ID: 5ce1ce425c97
+Revises: 11481f7b31a4
+Create Date: 2023-04-29 23:17:01.059190
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+from sqlalchemy import Enum
+import enum
+
+
+class EnrollmentStatus(enum.Enum):
+    ACTIVE = 1
+    DISABLED = 2
+
+
+enrollment_status = postgresql.ENUM(EnrollmentStatus, name="enrollment_status")
+
+# revision identifiers, used by Alembic.
+revision = "5ce1ce425c97"
+down_revision = "11481f7b31a4"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "enrollments",
+        sa.Column("id", sa.Integer(), nullable=False, primary_key=True),
+        sa.Column(
+            "student_id", sa.Integer(), sa.ForeignKey("users.id"), nullable=False
+        ),
+        sa.Column(
+            "course_id", sa.Integer(), sa.ForeignKey("courses.id"), nullable=False
+        ),
+        sa.Column("enrollment_date", sa.Date(), nullable=False),
+        sa.UniqueConstraint(
+            "student_id", "course_id", name="uq_student_course_enrollment"
+        ),
+    )
+    enrollment_status.create(op.get_bind())
+
+    op.add_column(
+        "enrollments",
+        sa.Column(
+            "status",
+            enrollment_status,
+            nullable=False,
+            server_default=EnrollmentStatus.ACTIVE.name,
+        ),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("enrollments")
+    enrollment_status.drop(op.get_bind())
+    pass
